80c65b8fa77568ea5620ebe15f2ea064883c577b
Balaji-Lee <balajilee2002@gmail.com>
2025-07-14 07:03:50 +0530

    add :: Remove Deplicates from An Array

---
+++ DSA/Array/RemoveDuplicatesfromAnArray.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+
+class RemoveDeplicatesfromAnArray{
+
+    public static void main(String[] arg){
+
+        int[] arr = {5, 2, 3,4, 2, 1, 4};
+
+        // By Using Array List
+        ArrayList<Integer> unique = new ArrayList<>();        
+        for(int i=0;i<arr.length;i++){
+            if(!unique.contains(arr[i])){
+                unique.add(arr[i]);
+            }
+        }
+        for(int v:unique){
+           System.out.println(v); 
+        }
+
+
+    
+    }
+
+}
