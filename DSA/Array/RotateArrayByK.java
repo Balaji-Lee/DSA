87474f6264c339ff57840776435799b21cf0e64c
Balaji L <balaji.l@bluetree.in>
2025-07-15 08:12:16 +0530

    Problem: Rotate Array by K Positions (Right Rotation)

---
+++ DSA/Array/RotateArrayByK.java
@@ -0,0 +1,28 @@
+class RotateArrayByK {
+
+    /*
+     * 🧪 Example:
+     * Input:
+     * arr = [1, 2, 3, 4, 5], k = 2
+     *
+     * Output:
+     * [4, 5, 1, 2, 3]
+     */
+
+    public static void main(String[] arg) {
+
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        int k = 6;
+
+        int[] resultArr = new int[arr.length];
+
+        for (int m = 0; m < arr.length; m++) {
+            resultArr[(m + k )% arr.length] = arr[m];
+        }
+
+        for (int o : resultArr) {
+            System.out.print(o);
+        }
+
+    }
+}
