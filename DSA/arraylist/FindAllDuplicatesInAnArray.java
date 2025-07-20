52d9f520bc5bba0fcbb5ca48b39f45400d8b9775
Balaji-Lee <balajilee2002@gmail.com>
2025-07-20 11:04:39 +0530

    problem : Find All Duplicates in an Array

--- DSA/arraylist/FindAllDuplicatesInAnArray.java
+++ DSA/arraylist/FindAllDuplicatesInAnArray.java
@@ -1,4 +1,34 @@
 package DSA.arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class FindAllDuplicatesInAnArray {
-    
+
+    // LEEDCODE
+    // https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1, 1, 11, 11, 100 };
+        System.out.println("Input :" + Arrays.toString(nums));
+        List<Integer> result = findDuplicates(nums);
+        System.out.println("Output :" + result);
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        HashSet<Integer> seen = new HashSet<>();
+        for (int n : nums) {
+            if (seen.contains(n)) {
+                if (!result.contains(n)) {
+                    result.add(n);
+                }
+            }else{
+                seen.add(n);
+            }
+        }
+        return result;
+    }
 }
