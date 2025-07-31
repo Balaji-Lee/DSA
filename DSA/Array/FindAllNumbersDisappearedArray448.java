3e2a5a2c1a99147bafa5c4b7e7c8005c447c552c
Balaji-Lee <balajilee2002@gmail.com>
2025-07-31 22:09:02 +0530

    fix : code optimized for N(0) time and N(1) space

--- DSA/Array/FindAllNumbersDisappearedArray448.java
+++ DSA/Array/FindAllNumbersDisappearedArray448.java
@@ -3,28 +3,32 @@ package DSA.array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class  FindAllNumbersDisappearedArray448{
+public class FindAllNumbersDisappearedArray448 {
 
-       public static void main(String[] args) {
+    // LEEDCODE https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
 
-        int[] arr = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
-       List<Integer>  result = findDisappearedNumbers(arr);
+    public static void main(String[] args) {
+
+        int[] arr = new int[] {4,3,2,7,8,2,3,1 };
+        List<Integer> result = findDisappearedNumbers(arr);
         System.out.println("Result :" + result.toString());
 
     }
 
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
 
-     public static  List<Integer> findDisappearedNumbers(int[] nums) {
-         List<Integer> orgin = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
-        for (int n : nums) {
-            orgin.add(n);
-        
+
+          for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
         }
 
-        for (int i = 1; i <= nums.length; i++) {
-            if (!orgin.contains(i)) {
-                result.add(i);
+          for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
             }
         }
 
