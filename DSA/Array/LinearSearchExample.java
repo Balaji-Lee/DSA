c0643bf9313b3316e718748eb08cb66e4d5b3bad
Balaji-Lee <balajilee2002@gmail.com>
2026-01-02 23:33:23 +0530

    chore : re-write the linearSearch

--- DSA/Array/LinearSearchExample.java
+++ DSA/Array/LinearSearchExample.java
@@ -1,26 +1,25 @@
-package DSA.array;
+package DSA.Array;
 public class LinearSearchExample {
 
     public static void main(String[] arg) {
-        int[] myArray = new int[]{1,2,3,4,55};
 
-        boolean isFivePresent =false;
+        int[] list = new int[]{1,2,44,55,66,67,69,88};
+        int targetVal =67;
 
-        for (int i = 0; i < myArray.length; i++) {
-            if(myArray[i]==5){
-                isFivePresent=true;
-                break;
-            }
-        }
-
-        if(isFivePresent){
-            System.out.println("Yes 5 present");
-        }else {
-            System.out.println("No 5 not present");
-        }
+        int targetPosition = findPosition(list, targetVal);
+        System.out.println("Target Position:"+ targetPosition);
 
     }
 
-// test
+    public static int findPosition(int[] array , int target){
+        int currentPosition =0;
+        for (int v : array){
+            if(v==target)
+                return currentPosition;
+            else
+                currentPosition++;
+        }
+        return 1;
+    }
 
 }
