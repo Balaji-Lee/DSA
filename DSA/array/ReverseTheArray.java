cea08d02ffe5c622abc8a290b9c523d01c28dfe2
Balaji-Lee <balajilee2002@gmail.com>
2026-03-02 19:05:16 +0530

    fix : printing the array in the readable formate

--- DSA/array/ReverseTheArray.java
+++ DSA/array/ReverseTheArray.java
@@ -1,5 +1,7 @@
 package DSA.array;
 
+import java.util.Arrays;
+
 public class ReverseTheArray {
 
       /**
@@ -15,21 +17,24 @@ public class ReverseTheArray {
     public static void reversTheArrayByTwoPointer(int[] arr){
         int start =0;
         int end = arr.length-1;
-
+        System.out.println("Start The Program");
         while(start != end){
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
+            start ++;
+            end --;
         }
 
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+
 
     }
 
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};  
-        reverseAndPrint(arr);
+//        reverseAndPrint(arr);
         reversTheArrayByTwoPointer(arr);
     }
 
